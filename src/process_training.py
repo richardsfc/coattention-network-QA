478c0cffdc7be830a9ac13409f512910cad366ed
ajermann <ajermann@gm.slc.edu>
2019-04-09 13:33:11 +0100

    Added L2 Norm

--- src/process_training.py
+++ src/process_training.py
@@ -138,6 +138,14 @@ class Processor(object):
         optimizer.zero_grad()
         q_seq, q_lens, d_seq, d_lens, span = self.get_data(batch)
         loss, _, _ = model(q_seq, q_lens, d_seq, d_lens, span)
+
+        l2_reg = None
+        for W in params:
+            if l2_reg is None:
+                l2_reg = W.norm(2)
+            else:
+                l2_reg = l2_reg + W.norm(2)
+        loss = loss + config.reg_lambda * l2_reg
         loss.backward()
 
         param_norm = self.get_param_norm(params)
