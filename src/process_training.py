2c599a6f1dd7eec4d89a0be671eab57786074775
sailordiary <me@caszhang.cn>
2019-04-09 15:34:52 +0800

    add adaptive parameter loading

--- src/process_training.py
+++ src/process_training.py
@@ -25,6 +25,20 @@ logging.basicConfig(level=logging.INFO)
 use_cuda = torch.cuda.is_available()
 print ('Using CUDA.' if use_cuda else 'Using CPU.')
 
+def adaptive_load_state_dict(model, state_dict):
+    own_state = model.state_dict()
+    for name, param in state_dict.items():
+        if name not in own_state:
+            print('Skipped {}, not found'.format(name))
+            continue
+        if isinstance(param, nn.Parameter):
+            # backwards compatibility for serialized parameters
+            param = param.data
+        if own_state[name].shape != param.shape:
+            print('Skipped {}, shape mismatch'.format(name))
+            continue
+        own_state[name].copy_(param)
+
 class Processor(object):
     def __init__(self):
         self.glove_path = os.path.join(config.data_dir, "glove.6B.{}d.txt".format(config.embedding_size))
@@ -98,9 +112,10 @@ class Processor(object):
 
         if model_file_path is not None:
             state = torch.load(model_file_path, map_location=lambda storage, location: storage)
-            model.load_state_dict(state['model'], strict=False)
+            adaptive_load_state_dict(state['model'])
 
         return model
+
     def get_grad_norm(self, parameters, norm_type=2):
         parameters = list(filter(lambda p: p.grad is not None, parameters))
         total_norm = 0
